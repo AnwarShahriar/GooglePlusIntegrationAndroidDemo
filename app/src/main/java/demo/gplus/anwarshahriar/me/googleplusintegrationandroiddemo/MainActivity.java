132ed9437bdad681e93be0f31e34a9ae93719e8b
anwarshahriar <anwarshahriar@gmail.com>
2014-12-30 10:50:32 -0800

    Sets up play service and GoogleApiClient

--- app/src/main/java/demo/gplus/anwarshahriar/me/googleplusintegrationandroiddemo/MainActivity.java
+++ app/src/main/java/demo/gplus/anwarshahriar/me/googleplusintegrationandroiddemo/MainActivity.java
@@ -1,39 +1,81 @@
 package demo.gplus.anwarshahriar.me.googleplusintegrationandroiddemo;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+    private GoogleApiClient apiClient;
+    private boolean intentInProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        apiClient.connect();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStop() {
+        super.onStop();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (apiClient.isConnected()) {
+            apiClient.disconnect();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        apiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!intentInProgress && result.hasResolution()) {
+            try {
+                intentInProgress = true;
+                startIntentSenderForResult(result.getResolution().getIntentSender(),
+                        0, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                intentInProgress = false;
+                apiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            intentInProgress = false;
+
+            if (!apiClient.isConnecting()) {
+                apiClient.connect();
+            }
+        }
     }
 }
